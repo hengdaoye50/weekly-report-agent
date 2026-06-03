48c11df451dbcb055a1daac84512a2a65d797371
hengdaoye50 <zjh2225491299@hotmail.com>
2026-06-03 10:36:53 +0800

    docs: 优化 README 排版和设计

--- src/main.py
+++ src/main.py
@@ -1,3 +1,11 @@
+import sys
+import io
+
+# Windows 终端中文显示
+if sys.platform == "win32":
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf-8", errors="replace")
+    sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding="utf-8", errors="replace")
+
 import click
 from src import config
 from src.agent.workflow import run
